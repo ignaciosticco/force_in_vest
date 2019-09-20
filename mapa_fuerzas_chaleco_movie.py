57af9838648c2d82a13a44dd293201202fcbffd1
ignaciosticco <ignaciosticco@gmail.com>
2019-09-20 14:25:55 -0300

    ahora los limites en x e y son parametros

--- mapa_fuerzas_chaleco_movie.py
+++ mapa_fuerzas_chaleco_movie.py
@@ -3,7 +3,6 @@ This script produces an animation of the temporal evolution of the density.
 The input file is the configuration file (positions for every timestep). 
 
 '''
- 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -37,25 +36,33 @@ params = {'backend': 'ps',
 
 pylab.rcParams.update(params)
 
+################### PARAMETERS ###################
+xi = 0.0
+xf = 1.0
+rows = 3
+yi = 0.0
+yf = 1.0
+cols = 4
+output_filename = 'chaleco1'
+##################################################
 
-# animation function
+
+
 def animate(i):
-    global cont
-    z = lista_grilla_fuerzas[i]
-    cont = plt.contourf(grid_x, grid_y, z, levels, cmap=plt.cm.jet,zorder=1)
-    #plt.title("Density map (p~m$^{-2}$) \t\t-\t\t N = %i" %lista_number_pedestrians[i]) 
-    #print(lista_number_pedestrians[i])
-    return cont
+	'''
+	Animation function
+	'''
+	global cont
+	z = lista_grilla_fuerzas[i]
+	cont = plt.contourf(grid_x, grid_y, z, levels, cmap=plt.cm.jet,zorder=1)
+	plt.title('Force map in the back ',fontsize=10)
+	return cont
+
 
 def grafica_bordes_chaleco():
   '''
   Grafica los bordes y las mangas del chaleco
   '''
-
-  # Lado derecho
-  plt.axvline(x=1,ymin=0.0,ymax=0.67,linewidth=1.5, color='k')
-  # Lado izquierdo
-  plt.axvline(x=0,ymin=0.0,ymax=0.67,linewidth=1.5, color='k')
 
   # Manga izquierda
   x1, y1 = [-0.5, -0.01], [0.8, 1]
@@ -65,7 +72,10 @@ def grafica_bordes_chaleco():
   x1, y1 = [1.01, 1.5], [1, 0.8]
   x2, y2 = [1.01, 1.5], [0.8, 0.6]
   plt.plot(x1, y1, x2, y2, linewidth=1.5,color='k')
-
+  # Borde derecho
+  plt.axvline(x=1,ymin=0.0,ymax=0.67,linewidth=1.5, color='k')
+  # Borde izquierdo
+  plt.axvline(x=0,ymin=0.0,ymax=0.67,linewidth=1.5, color='k')
 
 
 
@@ -75,7 +85,7 @@ def asigna_fuerzas(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12):
   reportados por cada sensor
   '''
   
-  grilla_fuerzas= np.zeros((3,4))
+  grilla_fuerzas= np.zeros((rows,cols)) # Inicializa con ceros 
   grilla_fuerzas[2,0]=s1
   grilla_fuerzas[2,1]=s2
   grilla_fuerzas[2,2]=s3
@@ -89,31 +99,28 @@ def asigna_fuerzas(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12):
   grilla_fuerzas[0,2]=s11
   grilla_fuerzas[0,3]=s12
 
+  # Faltan los sensores de los hombros.
+
   return grilla_fuerzas
 
 
 ################ MAIN ######################3
 
 
-sensor1=[100,200,300]
-sensor2=[90,80,100]
-sensor3=[120,350,30]
-sensor4=[89,90,73]
-sensor5=[112,234,130]
-sensor6=[90,84,50]
-sensor7=[50,120,73]
-sensor8=[87,230,37]
-sensor9=[4,200,20]
-sensor10=[0,80,134]
-sensor11=[2,330,73]
-sensor12=[12,90,74]
-
-
-time = 0 
-output_filename = 'chaleco1'
-
-grid_x = np.linspace(0,1,4)
-grid_y = np.linspace(0,1,3) 
+sensor1=[100,120,130]
+sensor2=[110,130,110]
+sensor3=[170,140,130]
+sensor4=[180,110,150]
+sensor5=[140,120,150]
+sensor6=[120,130,190]
+sensor7=[110,180,160]
+sensor8=[130,150,130]
+sensor9=[150,120,103]
+sensor10=[107,103,120]
+sensor11=[150,180,130]
+sensor12=[130,160,150]
+sensor13=[100,140,190]
+sensor14=[190,170,110]
 
 
 lista_grilla_fuerzas = []
@@ -126,128 +133,24 @@ for time in range(0,len(sensor1)):
   lista_grilla_fuerzas+=[grilla_fuerzas]
 
 total_frames = len(lista_grilla_fuerzas)
-
-grid_x = np.linspace(0,1,4)
-grid_y = np.linspace(0,1,3) 
+grid_x = np.linspace(xi,xf,cols)
+grid_y = np.linspace(yi,yf,rows) 
 grid_density0=lista_grilla_fuerzas[0]
 
 fig = plt.figure()
 ax = plt.axes(xlim=(-1, 1), ylim=(0, 1.2))
 grafica_bordes_chaleco()
 
-levels=np.linspace(10,np.max(grilla_fuerzas),70,endpoint=True)
-levbar=np.linspace(10,np.max(grilla_fuerzas),12,endpoint=True)
+levels=np.linspace(90,np.max(grilla_fuerzas),70,endpoint=True)
+levbar=np.linspace(20,np.max(grilla_fuerzas),12,endpoint=True)
 cont=plt.contourf(grid_x,grid_y,grid_density0, levels,cmap=plt.cm.jet)
 plt.colorbar(ticks=levbar)
 plt.grid(False)
 plt.xlabel('$x$-location~(m)',fontsize=10)
 plt.ylabel('$y$-location~(m)',fontsize=10)
-plt.title('Force map in the back ',fontsize=10)
 plt.xlim(-0.5,1.5)
 plt.ylim(0,1.2)
 plt.subplots_adjust(bottom=0.29,right=0.98,left=0.15)
 
 anim = animation.FuncAnimation(fig, animate, frames=total_frames, repeat=False)
-anim.save('animation_chaleco.mp4', writer=animation.FFMpegWriter())
-
-
-
-
-
-
-
-'''
-######################### PARAMETER #########################
-begin_corridor = 0.0
-end_corridor =  27.0
-wall_up = 24.0  
-wall_down = 0.0
-lenght_x = end_corridor-begin_corridor
-lenght_y = wall_up-wall_down
-#############################################################   
-
-
-def main_loop(data):
-    lista_grid_density = []
-    lista_number_pedestrians=[]
-    f=open(data, 'r')
-    lines=f.readlines()
-    line_number = 0
-    while line_number<len(lines):
-        line = lines[line_number].rstrip('\n')
-        if(line=='ITEM: NUMBER OF ATOMS'):
-            number_pedestrians=int(lines[line_number+1])
-            x=[]
-            y=[]
-            first_line_table=line_number+7
-            for j in range(first_line_table, first_line_table+number_pedestrians):
-                row=lines[j].split(' ')
-                x+=[float(row[1])]
-                y+=[float(row[2])]
-            
-            grid_density=process_density(x,y)
-            lista_grid_density+=[grid_density]
-            lista_number_pedestrians+=[number_pedestrians]
-            line_number+=number_pedestrians-1
-        line_number+=1
-    f.close()
-    return lista_grid_density,lista_number_pedestrians
-
-
-
-
-def process_density(x,y):
-    grid_x = np.linspace(begin_corridor,end_corridor,end_corridor-begin_corridor)
-    grid_y = np.linspace(wall_down,wall_up,wall_up-wall_down)
-    grid_N = np.zeros((len(grid_y),len(grid_x)))
-
-    for i in range(0,len(x)):
-        if (x[i]<end_corridor):
-            col=int((x[i]-begin_corridor)/(lenght_x)*(len(grid_x)))
-            fil = int((y[i]-wall_down)/(lenght_y)*(len(grid_y)))
-            grid_N[fil][col] += 1
-    return grid_N
-
-
-# animation function
-def animate(i):
-    global cont
-    z = lista_grid_density[i]
-    cont = plt.contourf(grid_x, grid_y, z, levels, cmap=plt.cm.jet,zorder=1)
-    plt.title("Density map (p~m$^{-2}$) \t\t-\t\t N = %i" %lista_number_pedestrians[i]) 
-    print(lista_number_pedestrians[i])
-    return cont
-
-
-
-data_config = input("Enter configurations file name:\n")
-lista_grid_density,lista_number_pedestrians = main_loop(data_config)
-
-grid_x = np.linspace(begin_corridor,end_corridor,end_corridor-begin_corridor)
-grid_y = np.linspace(wall_down,wall_up,wall_up-wall_down)
-grid_density0=lista_grid_density[0]
-
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 28), ylim=(0, 24))
-
-rect = patches.Rectangle((27,10.5),1,3,linewidth=1,edgecolor='g',facecolor='g')
-ax.add_patch(rect)
-
-
-levels=np.linspace(0,10,70,endpoint=True)
-levbar=np.linspace(0,10,11,endpoint=True)
-cont=plt.contourf(grid_x,grid_y,grid_density0, levels,cmap=plt.cm.jet)
-plt.colorbar(ticks=levbar)
-plt.xlabel('$x$-location~(m)',fontsize=8)
-plt.ylabel('$y$-location~(m)',fontsize=8)
-plt.yticks(np.linspace(0,wall_up,4), fontsize=8)
-plt.xticks(fontsize=8)
-plt.grid(False)
-plt.subplots_adjust(bottom=0.19,right=1.02,left=0.15)
-
-total_frames = len(lista_grid_density)
-
-anim = animation.FuncAnimation(fig, animate, frames=total_frames, repeat=False)
-anim.save('animation_density.mp4', writer=animation.FFMpegWriter())
-
-'''
+anim.save('{}.mp4'.format(output_filename), writer=animation.FFMpegWriter())
